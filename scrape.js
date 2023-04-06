2f4a27aabbe0237acfea6759112169132c918330
João Moreno <joao.moreno@microsoft.com>
2023-04-06 13:41:22 +0200

    first commit

---
+++ scrape.js
@@ -0,0 +1,45 @@
+const { graphql } = require('@octokit/graphql');
+
+async function main() {
+  const { result } = await graphql(
+    `{
+      repository(owner: "microsoft", name:"vscode") {
+        openIssues: issues(states:OPEN) {
+          totalCount
+        }
+        closedIssues: issues(states:CLOSED) {
+          totalCount
+        }
+        openBugs: issues(states:OPEN, labels:["bug"]) {
+          totalCount
+        }
+        closedBugs: issues(states:CLOSED, labels:["bug"]) {
+          totalCount
+        }
+        openFeatureRequests: issues(states:OPEN, labels:["feature-request"]) {
+          totalCount
+        }
+        closedFeatureRequests: issues(states:CLOSED, labels:["feature-request"]) {
+          totalCount
+        }
+        openPRs: pullRequests(states:OPEN) {
+          totalCount
+        }
+        closedPRs: pullRequests(states:CLOSED) {
+          totalCount
+        }
+        mergedPRs: pullRequests(states:MERGED) {
+          totalCount
+        }
+        stargazers {
+          totalCount
+        }
+      }
+    }`,
+    { headers: { authorization: `token ${process.env['GITHUB_TOKEN']}`, }, }
+  );
+
+  console.log(result);
+}
+
+main();
