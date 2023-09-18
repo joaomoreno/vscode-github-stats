f432ec908ea0a929f2f3877d1ca5e0c0da302d98
João Moreno <joao.moreno@microsoft.com>
2023-09-18 15:55:15 +0200

    clean

--- docs/main.js
+++ docs/main.js
@@ -10,26 +10,39 @@ import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm"
 // closedPRs
 // mergedPRs
 
-async function createPlot() {
+async function fetchData() {
   const blob = await fetch('data.jsonl.gz');
   const ds = new DecompressionStream('gzip');
   const os = blob.body.pipeThrough(ds);
   const res = new Response(os);
   const lines = await res.text();
-  const data = lines.split('\n').map(JSON.parse);
+  return lines.split('\n').map(JSON.parse);
+}
 
-  const plot = Plot.plot({
+function plot(data, year, y) {
+  const from = new Date(year);
+  const to = new Date(String(Number(year) + 1));
+  const x = d => new Date(d[0] * 1000);
+  const filter = d => d[0] * 1000 > from.getTime();
+
+  document.body.replaceChild(Plot.plot({
+    title: 'Open Issues',
+    x: {
+      domain: [from, to],
+      grid: true
+    },
     marks: [
-      Plot.areaY(data, { x: d => new Date(d[0] * 1000), y: "1", fillOpacity: 0.2 }),
-      Plot.lineY(data, { x: d => new Date(d[0] * 1000), y: "1" }),
+      Plot.ruleY([0]),
+      // Plot.areaY(data, { x, y, fillOpacity: 0.2 }),
+      Plot.lineY(data, { filter, x, y }),
+      Plot.crosshair(data, { x, y })
     ]
-  });
-
-  document.body.append(plot);
+  }), document.body.firstChild);
 }
 
 async function main() {
-  await createPlot();
+  const data = await fetchData();
+  plot(data, "2023", "1");
 }
 
 main();
